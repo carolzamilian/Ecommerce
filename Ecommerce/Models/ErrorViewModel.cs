4d625fbcaee996a52972f51b9b614f4e6b63235c
carolzamilian <carolzamilian@gmail.com>
2019-10-08 21:53:14 -0300

    Alteração no modelo

--- Ecommerce/Models/ErrorViewModel.cs
+++ Ecommerce/Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ using System;
 namespace Ecommerce.Models
 {
     public class ErrorViewModel
-    {
+    {//teste
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
